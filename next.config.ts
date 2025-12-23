2634778e1b1a4dc19a6fc849051428f999e6c5ea
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-12-24 00:00:11 +0200

    fix: Correct AVIF image filenames

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  // images: { unoptimized: true }
 };
 
 export default nextConfig;
